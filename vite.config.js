6db23e5c8eaed95f2e9d924d7bcf6c646e1438bb
HecthorDev <hectorgarciajobs19@gmail.com>
2025-01-20 17:22:36 -0600

    fix: update vite config for production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: 'http://localhost:5173/oracle-next-education/challenge-aluraflix/dist/',
   plugins: [react()],
 });
