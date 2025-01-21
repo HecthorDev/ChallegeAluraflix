3b0bcdc4be32187964e362fe1d93e57353c49a6f
HecthorDev <hectorgarciajobs19@gmail.com>
2025-01-20 19:35:03 -0600

    build: optimize production configuration with terser and chunk splitting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: '/AluraFlix/',
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'router': ['react-router-dom'],
+          'form-libs': ['formik', 'yup']
+        }
+      }
+    }
+  }
 });
