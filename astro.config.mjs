d8edea170d097c21c321aedcfff78c021aa66508
HecthorDev <hectorgarciajobs19@gmail.com>
2026-02-24 23:34:23 -0600

    feat: Configure Vite alias for SweetAlert2, optimize framer-motion, and refine package dependency management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
     integrations: [react(), tailwind()],
     vite: {
+        resolve: {
+            alias: {
+                'sweetalert2': 'sweetalert2/dist/sweetalert2.all.js',
+            }
+        },
         ssr: {
             noExternal: ['react-icons', 'react-multi-carousel', 'sweetalert2', 'framer-motion'],
         },
         optimizeDeps: {
-            include: ['react-multi-carousel', 'sweetalert2']
+            include: ['react-multi-carousel', 'sweetalert2', 'framer-motion']
         }
     }
 });
